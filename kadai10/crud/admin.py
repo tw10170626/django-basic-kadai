521027df6c57cd5bdf28d18894612c9fc6a522f3
tw10170626 <wt10179626@gmail.com>
2025-11-20 17:18:54 +0900

    Djangoの課題を提出2

--- kadai10/crud/admin.py
+++ kadai10/crud/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price')
+    list_display = ('id', 'name', 'price','category')
+    search_fields = ('name',)
+    list_filter = ('category',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
     search_fields = ('name',)
 
 # Register your models here.
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- kadai10/crud/models.py
+++ kadai10/crud/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.urls import reverse
-\
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
