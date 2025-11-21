fe6730caf4c957e3151d1b348abeecf345efa900
tw10170626 <wt10179626@gmail.com>
2025-11-21 17:43:37 +0900

    Djangoの課題を提出3

--- kadai10/crud/admin.py
+++ kadai10/crud/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Product, Category
+from django.utils.safestring import mark_safe
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price','category')
+    list_display = ('id', 'name', 'price','category','image')
     search_fields = ('name',)
     list_filter = ('category',)
+
+    def image(self, obj):
+        return mark_safe('<img src="{}" style="width:100px height:auto;">'.format(obj.img.url))
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
--- kadai10/crud/models.py
+++ kadai10/crud/models.py
@@ -12,6 +12,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.PositiveIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    img = models.ImageField(blank=True, default='noImage.png')
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
